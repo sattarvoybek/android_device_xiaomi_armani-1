7d4f5db1b08eff3185950188c8ad4d1ad3d11d86
tarunkapadia93 <tarunmyid@gmail.com>
2015-03-12 23:54:58 +0530

    Revert "Build ANT+ support"
    This reverts commit 16de004b9acbbc522793c5f4d28b463d56063533.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,9 +41,6 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_CUSTOM_BOOTIMG_MK := device/xiaomi/armani/mkbootimg.mk
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 
-# ANT+
-BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
-
 # Audio
 BOARD_USES_ALSA_AUDIO := true
 TARGET_QCOM_AUDIO_VARIANT := caf
