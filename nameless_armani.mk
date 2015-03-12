34212b35042c8356d8d237af821ab5f456c4a3e8
tarunkapadia93 <tarunmyid@gmail.com>
2015-03-13 05:18:37 +0530

    nameless RC2

--- nameless_armani.mk
+++ nameless_armani.mk
@@ -2,13 +2,13 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common nameless stuff
+$(call inherit-product, vendor/nameless/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
 PRODUCT_RELEASE_NAME := Redmi 1S
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := nameless_armani
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
